b4fc7773225c9a826c75ab4e3c863f9b18b7f68c
Vincent Giraud <vincent.giraud@teode.com>
2016-05-15 21:25:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Plugin for [YOURLS](http://yourls.org) `1.7+`. 
+
+Description
+-----------
+Prevents CORS issue with domain CNAMES and aliases for admin actions 
+
+Installation
+------------
+1. In `/user/plugins`, create a new folder named `access-control-allow-origin`.
+2. Drop these files in that directory.
+3. Go to the Plugins administration page ( *eg* `http://sho.rt/admin/plugins.php` ) and activate the plugin.
+
+License
+-------
+YOURLS' license, aka *"Do whatever the hell you want with it"*. 
+_YOURLS - MIT License_
---
+++ plugin.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Access Control Allow Origin
+Plugin URI: http://yourls.org/
+Description: Prevents CORS issue with domain CNAMES and aliases for admin actions
+Version: 1.0
+Author: Vincent Giraud
+Author URI: http://teode.com/
+*/
+// No direct call
+if( !defined( 'YOURLS_SITE' ) ) die();
+
+// Kick in if the loader does not recognize a valid pattern
+yourls_add_action( 'content_type_header', 'vg_yourls_cors' );
+
+function vg_yourls_cors($type) {
+    if( !headers_sent() ) {
+        $charset = yourls_apply_filter( 'content_type_header_charset', 'utf-8' );
+        $types = implode("; ", $type);
+        header( "Content-Type: $types; charset=$charset" );
+        $domain = parse_url(YOURLS_SITE);
+        $fqdn = $domain['scheme']."://".$domain['host'];
+        header("Access-Control-Allow-Origin: "+$fqdn);
+        return true;
+    }
+    return false;
+}
+?>
